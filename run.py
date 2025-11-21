3ea42a0f65ab4748c7cd6b9d1f91b714a49b5202
Jorge Gaitan <info.dinnovos@gmail.com>
2025-11-21 18:15:53 -0500

    path realod exludes in run.py

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "src.main:app",
         host="0.0.0.0",
+        reload_excludes=[".venv/*", "venv/*", ".git/*", "__pycache__/*", "*.pyc"],
         port=8000,
         reload=True,
         log_level="info",
