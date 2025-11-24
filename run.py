9fa72aab771374621b45a05fa8572f1c67d1b5ff
Jorge Gaitan <info.dinnovos@gmail.com>
2025-11-24 07:36:21 -0500

    new path for univorn watch, reload_dirs: src, agents

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "src.main:app",
         host="0.0.0.0",
-        reload_excludes=[".venv/*", "venv/*", ".git/*", "__pycache__/*", "*.pyc"],
+        reload_dirs=["src", "agents"],
         port=8000,
         reload=True,
         log_level="info",
